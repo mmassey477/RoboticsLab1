81e472348cac573375a1cc04572669279f3d4eb5
Zoolouie <zander@bugsplat.com>
2018-09-05 17:32:58 -0600

    finished

--- sparki.ino
+++ sparki.ino
@@ -2,10 +2,17 @@
 #define STATE_FIND_OBJ 0
 #define STATE_DRIVE_TO_OBJ 1
 #define DETECT_LINE 2
+#define FOLLOW_LINE 3
+#define DROP_OBJECT 4
 
 int currentState = 0;
 int distanceToObject = 30;
 int distanceBeforeObject  = 7;
+
+int line_left = 1000;
+int line_center = 1000;
+int line_right = 1000;
+int threshhold = 500;
 
 void setup() {
   // put your setup code here, to run once:
@@ -24,18 +31,21 @@ void loop() {
 int distance = -1;
 switch(currentState) {
   case STATE_FIND_OBJ:
-    sparki.moveRight(5);
+    sparki.moveRight();
     distance = sparki.ping();
     if (distance > 0 && distance < 30){
-      sparki.moveRight(10);
+      sparki.moveStop();
       currentState = 1;
     }
     break;
   case STATE_DRIVE_TO_OBJ:
-    sparki.moveForward(1);
+    sparki.moveForward();
     distance = sparki.ping();
-    delay(1000);
     if(distance <= 5 && distance > 0){
+     sparki.moveStop();
+     delay(1000);
+     sparki.moveForward(1);
+     sparki.moveStop();
      sparki.gripperClose();
      delay(5000);
      sparki.gripperStop();
@@ -45,12 +55,47 @@ switch(currentState) {
     }
     break;
    case DETECT_LINE:
-    sparki.moveForward(1);
-    if (readSensors()){
+    sparki.moveForward();
+    delay(1000);
+    readSensors();
+    if (line_center < threshhold){
+      sparki.moveRight();
+      delay(1000);
       currentState = 3;
+      break;
     }
     
     break;
+
+   case FOLLOW_LINE:
+    readSensors();
+    if (line_left < threshhold){
+      sparki.moveLeft();
+    }
+    if (line_right < threshhold) {
+      sparki.moveRight();
+    }
+    if (line_center < threshhold) {
+      sparki.moveForward();
+    }
+    if ((line_center < threshhold) && (line_left > threshhold) && (line_right > threshhold)){
+      sparki.moveForward();
+    }
+    if ((line_center < threshhold) && (line_left < threshhold) && (line_right < threshhold)){
+      currentState = DROP_OBJECT;
+    }
+    
+    break;
+   case DROP_OBJECT:
+    sparki.moveStop();
+    delay(1000);
+    sparki.beep();
+    delay(1000);
+    sparki.gripperOpen();
+    delay(5000);
+    break;
+    
+    
   default:
   //Panic!
     break;
@@ -58,13 +103,8 @@ switch(currentState) {
 
 }
 
-bool readSensors() {
-//cm_distance = sparki.ping();
-int line_left = sparki.lightLeft();
-int line_right = sparki.lightRight();
-int line_center = sparki.lightCenter();
-if (line_center < 500){
-  return true;
-}
-return false;
+void readSensors() {
+line_left = sparki.lineLeft();
+line_right = sparki.lineRight();
+line_center = sparki.lineCenter();
 }
